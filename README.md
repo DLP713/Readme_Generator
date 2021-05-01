4337adb9d2188888c73a0477c6a874d918bd889a
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-05-01 18:07:05 -0500

    edited files to allow for readme creation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Readme_Generator
+# Professional ReadMe Generator
+
+  ## Description
+  Generates a ReadMe file when prompted in the terminal
+
+  ---
+
+  ## Installation
+  see video
+
+  ---
+
+  ## Contributors
+  CD
+
+  ---
+
+  ## Github
+  DLP713
--- index.js
+++ index.js
@@ -1,91 +1,56 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require("util");
+const writeFileAsync = util.promisify(writeToFile);
 // TODO: Create an array of questions for user input
-inquirer.prompt(
-    [
-        {
-            type: 'input',
-            message: 'Enter your project title:',
-            name: 'title',
-            // Checks if user added a value
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'Describe your project:',
-            name: 'description',
-            // Checks if user added a value
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'Installation instructions:',
-            name: 'instructions',
-            // Checks if user added a value
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        // {
-        //     type: 'list',
-        //     message: 'What license did you use?',
-        //     name: 'license',
-        //     choices: ['MIT license, GPL license, Apache license, GNU license, N/A']
-        //     // Checks if user added a value
-        //     validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        // },
-        {
-            type: 'input',
-            message: 'List the names of contributers separated by a comma:',
-            name: 'contributors',
-            // Checks if user added a value
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'Github username:',
-            name: 'github',
-            // Checks if user added a value
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        }
-    ]
-).then(({
-    title,
-    description,
-    instructions,
-    contributors,
-    github
-})=>{
-    
-const data = `# ${title}`
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project:',
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'List the names of contributers separated by a comma:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Github username:',
+    }
+];
 
-* [Description][#description]
-* [Installation][#instructions]
-* [Contributors][#contributors]
-* [Github][#github]
-# Description
-${description}
-## Installation
-${instructions}
-## Contributors
-${contributors}
-## Github
-${github};
-
-createReadMe(title,data);
-}
-);
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(`./${filename.toLowercase().split('').joint('')}.md`, data()=>(err){
+    fs.writeFile(fileName, data, err=>{
         if(err){
-            console.log(err)
+            console.log(err);
         }
-        console.log('Your README has been created');
-    })
-}
+        console.log('Your README has been created')
+    });
+};   
 
-// TODO: Create a function to initialize app
-function init() {}
+async function init(){
+    try {
+        const userAnswers = await inquirer.prompt(questions);
+        const markdown = generateMarkdown(userAnswers);
+        await writeFileAsync('README.md', markdown);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-// Function call to initialize app
 init();
+
