8af216552853dbc56c681a1d99a2780d6269f1e7
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-05-01 16:31:48 -0500

    adding prompts

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [
-    'Enter your project title:',
-    'Describe your project:',
-    'Installation instructions:',
-    'List the names of contributers separated by a comma:',
-    'Github username:'
-];
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            message: 'Enter your project title:',
+            name: 'title',
+            // Checks if user added a value
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message: 'Describe your project:',
+            name: 'description',
+            // Checks if user added a value
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message: 'Installation instructions:',
+            name: 'instructions',
+            // Checks if user added a value
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        // {
+        //     type: 'list',
+        //     message: 'What license did you use?',
+        //     name: 'license',
+        //     choices: ['MIT license, GPL license, Apache license, GNU license, N/A']
+        //     // Checks if user added a value
+        //     validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        // },
+        {
+            type: 'input',
+            message: 'List the names of contributers separated by a comma:',
+            name: 'contributors',
+            // Checks if user added a value
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message: 'Github username:',
+            name: 'github',
+            // Checks if user added a value
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        }
+    ]
+).then(({
+    title,
+    description,
+    instructions,
+    contributors,
+    github
+}
+
+))
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
